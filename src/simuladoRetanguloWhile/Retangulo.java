5f9dd49509057a37235c73a5d61f86862f74acc7
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-20 19:28:49 -0300

    Leitura da largura com validacao

--- src/simuladoRetanguloWhile/Retangulo.java
+++ src/simuladoRetanguloWhile/Retangulo.java
@@ -1,9 +1,25 @@
 package simuladoRetanguloWhile;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Retangulo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		// leitura da largura com validacao
+		int largura = 0, altura = 0; 
+		System.out.print("Digite a largura do retangulo: ");
+		largura = sc.nextInt();
+		while (largura <=0) {
+			System.out.print("O valor deve ser positivo. Tente novamente: ");
+			largura = sc.nextInt();
+		}
+		
+		
+		sc.close();
 
 	}
 
